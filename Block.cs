0abd617a11e6760330e37d470e47a683de4d8d83
Tomaso <66296094+IsaakThaddaeus@users.noreply.github.com>
2025-02-19 23:54:11 +0100

    Add files via upload

--- Block.cs
+++ Block.cs
@@ -1,10 +1,13 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
     public int health = 100;
     public bool grounded = false;
+    public List<GameObject> fractureModels = new List<GameObject>();
     DestructionManager destructionManager;
+    
 
     void Start()
     {
@@ -24,6 +27,14 @@ public class Block : MonoBehaviour
     public void destroy()
     {
         destructionManager.destroyBlock(this);
+      
+        GameObject fracturedBlock = Instantiate(fractureModels[Random.Range(0, fractureModels.Count)], transform.position, transform.rotation);
+        foreach (Transform child in fracturedBlock.transform)
+        {
+            child.gameObject.GetComponent<Rigidbody>().AddExplosionForce(1000, transform.position, 10);
+            Destroy(child.gameObject, 10);
+        }
+        
         Destroy(gameObject);
 
         //Stuff to visualize the destruction
